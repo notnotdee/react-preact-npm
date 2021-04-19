ce9b901915016577fc4ca6442d31e4a246dfcfb7
dl-watson <dlarkinwatson@gmail.com>
2021-04-19 12:32:06 -0700

    add style-loader dep, add example style config

--- react/src/components/app/App.jsx
+++ react/src/components/app/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import styles from './styles/App.css';
 
 const App = () => {
-  return <div>Hello World</div>;
+  return <div className={styles.App}>Hello World</div>;
 };
 
 export default App;
